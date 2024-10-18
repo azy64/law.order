56c6c367e2f8e567275f1b7b9992f400b00f0158
azaria <allysaidi64@gmail.com>
2024-10-18 14:15:41 +0200

    Remove unused import package

--- src/main/java/com/tunaweza/law/order/controller/SpecialisationController.java
+++ src/main/java/com/tunaweza/law/order/controller/SpecialisationController.java
@@ -4,14 +4,12 @@
  */
 package com.tunaweza.law.order.controller;
 
-import com.tunaweza.law.order.repository.SpecialisationRepository;
 import com.tunaweza.law.order.model.Specialisation;
 import com.tunaweza.law.order.service.EntityService;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +17,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.tunaweza.law.order.service.SpecialisationService;
 
 /**
  *
